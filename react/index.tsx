689f3f9f2827de62b940fddef638f189c595c18c
Bruno Moreira <bruno@brunormoreira.com>
2020-09-14 15:11:15 -0300

    Fix interface name

--- react/index.tsx
+++ react/index.tsx
@@ -1,7 +1,32 @@
 import { canUseDOM } from 'vtex.render-runtime'
 
-export function handleEvents() {}
+import push from './modules/push'
+import { getProductData } from './modules/product'
+
+export default function () {
+  return null
+} // no-op for extension point
+
+export function handleLegacyEvents(e: PixelMessage) {
+  let data
+
+  switch (e.data.eventName) {
+    case 'vtex:productView': {
+      data = { ...getProductData(e.data) }
+      break
+    }
+
+    default:
+      return
+  }
+
+  push({
+    event: 'legacy:productDetail',
+    transactionDate: new Date().getTime(),
+    ecommerce: { details: { products: [{ ...data }] } },
+  })
+}
 
 if (canUseDOM) {
-  window.addEventListener('message', handleEvents)
+  window.addEventListener('message', handleLegacyEvents)
 }
